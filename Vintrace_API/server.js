fff99db26970b66a71a07da56e34b5a132cfb52f
Nandy Rodrigo <nandy.rodrigo@gmail.com>
2020-10-27 10:00:54 +1100

    server routing made dynamic

--- Vintrace_API/server.js
+++ Vintrace_API/server.js
@@ -27,259 +27,117 @@ app.get("/15MPPN002-VK", function (req, res) {
 
 // breakdown "year" data
 
-app.get("/api/breakdown/year/11YVCHAR001", function (req, res) {
-  fs.readFile("data/11YVCHAR001.json", function (err, data) {
-    var object = JSON.parse(data);
-    var components = object.components;
+app.get('/api/breakdown/year/:vintraceID', function (req, res) {
+  var resourceID = req.params.vintraceID // get the ID from the request object
+
+  fs.readFile(`data/${resourceID}.json`, function (err, data) { // use the id to dynamically find json file
+    if (err) { // if the client sends through something that doesn't match return a 404
+      res.status(404).send("Sorry, we can't find that")
+    }
+
+    var object = JSON.parse(data)
+    var components = object.components
     // sort component data from highest percentage to lowest percentage
-    components.sort((a, b) => (b.percentage > a.percentage) ? 1 : -1)
+    components.sort((a, b) => (b.percentage > a.percentage ? 1 : -1))
     // remove unwanted fields
-    components.forEach(element => {
-      delete element["variety"]
-      delete element["region"]
-    });
-    var breakDownByYear = {};
+    components.forEach((element) => {
+      delete element['variety']
+      delete element['region']
+    })
+    var breakDownByYear = {}
     // add "breakdown type" key value pair
-    breakDownByYear.breakDownType = "year"
+    breakDownByYear.breakDownType = 'year'
     // add "breakdown" key value pair where breakdown = components
     breakDownByYear.breakdown = components
-    console.log(breakDownByYear);
-    res.end(JSON.stringify(breakDownByYear));
-  });
+    console.log(breakDownByYear)
+    res.end(JSON.stringify(breakDownByYear))
+  })
 });
 
-app.get("/api/breakdown/year/11YVCHAR002", function (req, res) {
-  fs.readFile("data/11YVCHAR002.json", function (err, data) {
-    var object = JSON.parse(data);
-    var components = object.components;
+// // breakdown "variety" data
+
+app.get('/api/breakdown/variety/:vintraceID', function (req, res) {
+  var resourceID = req.params.vintraceID // get the ID from the request object
+
+  fs.readFile(`data/${resourceID}.json`, function (err, data) { // use the id to dynamically find json file
+    if (err) { // if the client sends through something that doesn't match return a 404
+      res.status(404).send("Sorry, we can't find that")
+    }
+
+    var object = JSON.parse(data)
+    var components = object.components
     // sort component data from highest percentage to lowest percentage
-    components.sort((a, b) => (b.percentage > a.percentage) ? 1 : -1)
+    components.sort((a, b) => (b.percentage > a.percentage ? 1 : -1))
     // remove unwanted fields
-    components.forEach(element => {
-      delete element["variety"]
-      delete element["region"]
-    });
-    var breakDownByYear = {};
+    components.forEach((element) => {
+      delete element['year']
+      delete element['region']
+    })
+    var breakDownByYear = {}
     // add "breakdown type" key value pair
-    breakDownByYear.breakDownType = "year"
+    breakDownByYear.breakDownType = 'variety'
     // add "breakdown" key value pair where breakdown = components
     breakDownByYear.breakdown = components
-    console.log(breakDownByYear);
-    res.end(JSON.stringify(breakDownByYear));
-  });
+    console.log(breakDownByYear)
+    res.end(JSON.stringify(breakDownByYear))
+  })
 });
 
-app.get("/api/breakdown/year/15MPPN002-VK", function (req, res) {
-  fs.readFile("data/15MPPN002-VK.json", function (err, data) {
-    var object = JSON.parse(data);
-    var components = object.components;
+// // breakdown "region" data
+
+app.get('/api/breakdown/region/:vintraceID', function (req, res) {
+  var resourceID = req.params.vintraceID // get the ID from the request object
+
+  fs.readFile(`data/${resourceID}.json`, function (err, data) { // use the id to dynamically find json file
+    if (err) { // if the client sends through something that doesn't match return a 404
+      res.status(404).send("Sorry, we can't find that")
+    }
+
+    var object = JSON.parse(data)
+    var components = object.components
     // sort component data from highest percentage to lowest percentage
-    components.sort((a, b) => (b.percentage > a.percentage) ? 1 : -1)
+    components.sort((a, b) => (b.percentage > a.percentage ? 1 : -1))
     // remove unwanted fields
-    components.forEach(element => {
-      delete element["variety"]
-      delete element["region"]
-    });
-    var breakDownByYear = {};
+    components.forEach((element) => {
+      delete element['year']
+      delete element['variety']
+    })
+    var breakDownByYear = {}
     // add "breakdown type" key value pair
-    breakDownByYear.breakDownType = "year"
+    breakDownByYear.breakDownType = 'region'
     // add "breakdown" key value pair where breakdown = components
     breakDownByYear.breakdown = components
-    console.log(breakDownByYear);
-    res.end(JSON.stringify(breakDownByYear));
-  });
+    console.log(breakDownByYear)
+    res.end(JSON.stringify(breakDownByYear))
+  })
 });
 
-// breakdown "variety" data
+// // breakdown "year-variety" data
 
-app.get("/api/breakdown/variety/11YVCHAR001", function (req, res) {
-  fs.readFile("data/11YVCHAR001.json", function (err, data) {
-    var object = JSON.parse(data);
-    var components = object.components;
+app.get('/api/breakdown/year-variety/:vintraceID', function (req, res) {
+  var resourceID = req.params.vintraceID // get the ID from the request object
+
+  fs.readFile(`data/${resourceID}.json`, function (err, data) { // use the id to dynamically find json file
+    if (err) { // if the client sends through something that doesn't match return a 404
+      res.status(404).send("Sorry, we can't find that")
+    }
+
+    var object = JSON.parse(data)
+    var components = object.components
     // sort component data from highest percentage to lowest percentage
-    components.sort((a, b) => (b.percentage > a.percentage) ? 1 : -1)
+    components.sort((a, b) => (b.percentage > a.percentage ? 1 : -1))
     // remove unwanted fields
-    components.forEach(element => {
-      delete element["year"]
-      delete element["region"]
-    });
-    var breakDownByVariety = {};
+    components.forEach((element) => {
+      delete element['region']
+    })
+    var breakDownByYear = {}
     // add "breakdown type" key value pair
-    breakDownByVariety.breakDownType = "variety"
+    breakDownByYear.breakDownType = 'year-variety'
     // add "breakdown" key value pair where breakdown = components
-    breakDownByVariety.breakdown = components
-    console.log(breakDownByVariety);
-    res.end(JSON.stringify(breakDownByVariety));
-  });
-});
-
-app.get("/api/breakdown/variety/11YVCHAR002", function (req, res) {
-  fs.readFile("data/11YVCHAR002.json", function (err, data) {
-    var object = JSON.parse(data);
-    var components = object.components;
-    // sort component data from highest percentage to lowest percentage
-    components.sort((a, b) => (b.percentage > a.percentage) ? 1 : -1)
-    // remove unwanted fields
-    components.forEach(element => {
-      delete element["year"]
-      delete element["region"]
-    });
-    var breakDownByVariety = {};
-    // add "breakdown type" key value pair
-    breakDownByVariety.breakDownType = "variety"
-    // add "breakdown" key value pair where breakdown = components
-    breakDownByVariety.breakdown = components
-    console.log(breakDownByVariety);
-    res.end(JSON.stringify(breakDownByVariety));
-  });
-});
-
-app.get("/api/breakdown/variety/15MPPN002-VK", function (req, res) {
-  fs.readFile("data/15MPPN002-VK.json", function (err, data) {
-    var object = JSON.parse(data);
-    var components = object.components;
-    // sort component data from highest percentage to lowest percentage
-    components.sort((a, b) => (b.percentage > a.percentage) ? 1 : -1)
-    // remove unwanted fields
-    components.forEach(element => {
-      delete element["year"]
-      delete element["region"]
-    });
-    var breakDownByVariety = {};
-    // add "breakdown type" key value pair
-    breakDownByVariety.breakDownType = "variety"
-    // add "breakdown" key value pair where breakdown = components
-    breakDownByVariety.breakdown = components
-    console.log(breakDownByVariety);
-    res.end(JSON.stringify(breakDownByVariety));
-  });
-});
-
-// breakdown "region" data
-
-app.get("/api/breakdown/region/11YVCHAR001", function (req, res) {
-  fs.readFile("data/11YVCHAR001.json", function (err, data) {
-    var object = JSON.parse(data);
-    var components = object.components;
-    // sort component data from highest percentage to lowest percentage
-    components.sort((a, b) => (b.percentage > a.percentage) ? 1 : -1)
-    // remove unwanted fields
-    components.forEach(element => {
-      delete element["year"]
-      delete element["variety"]
-    });
-    var breakDownByRegion = {};
-    // add "breakdown type" key value pair
-    breakDownByRegion.breakDownType = "region"
-    // add "breakdown" key value pair where breakdown = components
-    breakDownByRegion.breakdown = components
-    console.log(breakDownByRegion);
-    res.end(JSON.stringify(breakDownByRegion));
-  });
-});
-
-app.get("/api/breakdown/region/11YVCHAR002", function (req, res) {
-  fs.readFile("data/11YVCHAR002.json", function (err, data) {
-    var object = JSON.parse(data);
-    var components = object.components;
-    // sort component data from highest percentage to lowest percentage
-    components.sort((a, b) => (b.percentage > a.percentage) ? 1 : -1)
-    // remove unwanted fields
-    components.forEach(element => {
-      delete element["year"]
-      delete element["variety"]
-    });
-    var breakDownByRegion = {};
-    // add "breakdown type" key value pair
-    breakDownByRegion.breakDownType = "region"
-    // add "breakdown" key value pair where breakdown = components
-    breakDownByRegion.breakdown = components
-    console.log(breakDownByRegion);
-    res.end(JSON.stringify(breakDownByRegion));
-  });
-});
-
-app.get("/api/breakdown/region/15MPPN002-VK", function (req, res) {
-  fs.readFile("data/15MPPN002-VK.json", function (err, data) {
-    var object = JSON.parse(data);
-    var components = object.components;
-    // sort component data from highest percentage to lowest percentage
-    components.sort((a, b) => (b.percentage > a.percentage) ? 1 : -1)
-    // remove unwanted fields
-    components.forEach(element => {
-      delete element["year"]
-      delete element["variety"]
-    });
-    var breakDownByRegion = {};
-    // add "breakdown type" key value pair
-    breakDownByRegion.breakDownType = "region"
-    // add "breakdown" key value pair where breakdown = components
-    breakDownByRegion.breakdown = components
-    console.log(breakDownByRegion);
-    res.end(JSON.stringify(breakDownByRegion));
-  });
-});
-
-// breakdown "year-variety" data
-
-app.get("/api/breakdown/year-variety/11YVCHAR001", function (req, res) {
-  fs.readFile("data/11YVCHAR001.json", function (err, data) {
-    var object = JSON.parse(data);
-    var components = object.components;
-    // sort component data from highest percentage to lowest percentage
-    components.sort((a, b) => (b.percentage > a.percentage) ? 1 : -1)
-    // remove unwanted fields
-    components.forEach(element => {
-      delete element["region"]
-    });
-    var breakDownByYearVariety = {};
-    // add "breakdown type" key value pair
-    breakDownByYearVariety.breakDownType = "year-variety"
-    // add "breakdown" key value pair where breakdown = components
-    breakDownByYearVariety.breakdown = components
-    console.log(breakDownByYearVariety);
-    res.end(JSON.stringify(breakDownByYearVariety));
-  });
-});
-
-app.get("/api/breakdown/year-variety/11YVCHAR002", function (req, res) {
-  fs.readFile("data/11YVCHAR002.json", function (err, data) {
-    var object = JSON.parse(data);
-    var components = object.components;
-    // sort component data from highest percentage to lowest percentage
-    components.sort((a, b) => (b.percentage > a.percentage) ? 1 : -1)
-    // remove unwanted fields
-    components.forEach(element => {
-      delete element["region"]
-    });
-    var breakDownByYearVariety = {};
-    // add "breakdown type" key value pair
-    breakDownByYearVariety.breakDownType = "year-variety"
-    // add "breakdown" key value pair where breakdown = components
-    breakDownByYearVariety.breakdown = components
-    console.log(breakDownByYearVariety);
-    res.end(JSON.stringify(breakDownByYearVariety));
-  });
-});
-
-app.get("/api/breakdown/year-variety/15MPPN002-VK", function (req, res) {
-  fs.readFile("data/15MPPN002-VK.json", function (err, data) {
-    var object = JSON.parse(data);
-    var components = object.components;
-    // sort component data from highest percentage to lowest percentage
-    components.sort((a, b) => (b.percentage > a.percentage) ? 1 : -1)
-    // remove unwanted fields
-    components.forEach(element => {
-      delete element["region"]
-    });
-    var breakDownByYearVariety = {};
-    // add "breakdown type" key value pair
-    breakDownByYearVariety.breakDownType = "year-variety"
-    // add "breakdown" key value pair where breakdown = components
-    breakDownByYearVariety.breakdown = components
-    console.log(breakDownByYearVariety);
-    res.end(JSON.stringify(breakDownByYearVariety));
-  });
+    breakDownByYear.breakdown = components
+    console.log(breakDownByYear)
+    res.end(JSON.stringify(breakDownByYear))
+  })
 });
 
 // app running on http://localhost:3000/
